aa4b9d2c50cb009635239ac83f4c02380bc7d361
Jean Hanna <jeanhanna1234567@gmail.com>
2024-07-17 23:24:03 -0700

    Convert apiRequest to <Map,Map>

--- src/org/john/api/APIRequestHandler.java
+++ src/org/john/api/APIRequestHandler.java
@@ -6,36 +6,35 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class APIRequestHandler {
-    private final Map<RequestMethod, List<APIRequest>> apiRequests = new ConcurrentHashMap<>();
+    private final Map<RequestMethod, Map<String, APIRequest>>
+                                                apiRequests = new ConcurrentHashMap<>();
 
     public APIRequestHandler() {
-        apiRequests.put(RequestMethod.GET, new ArrayList<>());
-        apiRequests.put(RequestMethod.POST, new ArrayList<>());
-        apiRequests.put(RequestMethod.PUT, new ArrayList<>());
-        apiRequests.put(RequestMethod.DELETE, new ArrayList<>());
+        apiRequests.put(RequestMethod.GET, new HashMap<>());
+        apiRequests.put(RequestMethod.POST, new HashMap<>());
+        apiRequests.put(RequestMethod.PUT, new HashMap<>());
+        apiRequests.put(RequestMethod.DELETE, new HashMap<>());
     }
 
     public Optional<APIRequest> getAPI(RequestMethod request, String path) {
-        return apiRequests.get(request)
-                .stream()
-                .filter(Objects::nonNull)
-                .filter(it -> it.path().equalsIgnoreCase(path))
-                .findFirst();
+        return apiRequests.get(request).containsKey(path)
+                ? Optional.of(apiRequests.get(request).get(path))
+                : Optional.empty();
     }
 
     public void doGet(String path, APIRequestExecutor apiRequestExecutor) {
-        apiRequests.get(RequestMethod.GET).add(new APIRequest(path, apiRequestExecutor));
+        apiRequests.get(RequestMethod.GET).put(path, new APIRequest(path, apiRequestExecutor));
     }
 
     public void doPost(String path, APIRequestExecutor apiRequestExecutor) {
-        apiRequests.get(RequestMethod.POST).add(new APIRequest(path, apiRequestExecutor));
+        apiRequests.get(RequestMethod.POST).put(path, new APIRequest(path, apiRequestExecutor));
     }
 
     public void doPut(String path, APIRequestExecutor apiRequestExecutor) {
-        apiRequests.get(RequestMethod.PUT).add(new APIRequest(path, apiRequestExecutor));
+        apiRequests.get(RequestMethod.PUT).put(path, new APIRequest(path, apiRequestExecutor));
     }
 
     public void doDelete(String path, APIRequestExecutor apiRequestExecutor) {
-        apiRequests.get(RequestMethod.DELETE).add(new APIRequest(path, apiRequestExecutor));
+        apiRequests.get(RequestMethod.DELETE).put(path, new APIRequest(path, apiRequestExecutor));
     }
 }
